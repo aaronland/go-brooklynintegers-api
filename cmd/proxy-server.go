df5f0b574ed31c7c5a5ef4e1e29fa7cc0998f711
thisisaaronland <aaron@mapzen.com>
2015-12-12 02:45:29 +0000

    move status and monitoring in to their own functions; better throttling; lots of comments

--- cmd/proxy-server.go
+++ cmd/proxy-server.go
@@ -20,6 +20,7 @@ type Proxy struct {
 	client  *api.APIClient
 	pool    *pool.LIFOPool
 	minpool int64
+	refill  chan bool
 }
 
 func NewProxy(min_pool int64, logger *log.WOFLogger) *Proxy {
@@ -27,11 +28,24 @@ func NewProxy(min_pool int64, logger *log.WOFLogger) *Proxy {
 	api_client := api.NewAPIClient()
 	pool := pool.NewLIFOPool()
 
+	// See notes in RefillPool() for details
+
+	size := 10
+	refill := make(chan bool, 10)
+
+	for i := 0; i < size; i++ {
+		refill <- true
+	}
+
+	// Possibly also keep global stats on number of fetches
+	// and cache hits/misses/etc
+
 	proxy := Proxy{
 		logger:  logger,
 		client:  api_client,
 		pool:    pool,
 		minpool: min_pool,
+		refill:  refill,
 	}
 
 	return &proxy
@@ -41,43 +55,119 @@ func (p *Proxy) Init() {
 
 	go p.RefillPool()
 
-	go func() {
-		p.Monitor()
-	}()
+	go p.Status()
+	go p.Monitor()
+}
+
+func (p *Proxy) Status() {
+
+	for {
+		select {
+		case <-time.After(5 * time.Second):
+			p.logger.Status("pool length: %d", p.pool.Length())
+		}
+	}
 }
 
 func (p *Proxy) Monitor() {
 
 	for {
-
-		p.logger.Debug("pool: %d", p.pool.Length())
-
-		if p.pool.Length() < p.minpool {
-			p.RefillPool()
+		select {
+		case <-time.After(10 * time.Second):
+			if p.pool.Length() < p.minpool {
+				go p.RefillPool()
+			}
 		}
 
-		time.Sleep(1 * time.Second)
 	}
 }
 
 func (p *Proxy) RefillPool() {
 
-	wg := new(sync.WaitGroup)
+	// Remember there is a fixed size work queue of allowable times to try
+	// and refill the pool simultaneously. First, we block until a slot opens
+	// up.
+
+	<-p.refill
+
+	t1 := time.Now()
+
+	// Figure out how many integers we need to get *at this moment* which when
+	// the service is under heavy load is a misleading number at best. It might
+	// be worth adjusting this by a factor of (n) depending on the current load.
+	// But that also means tracking what we think the current load means so we
+	// aren't going to do that now...
 
 	todo := p.minpool - p.pool.Length()
+	workers := int(p.minpool / 2)
+
+	if workers == 0 {
+		workers = 1
+	}
+
+	// Now we're going to set up two simultaneous queues. One (the work group) is
+	// just there to keep track of all the requests for new integers we need to
+	// make. The second (the throttle) is there to make sure we don't exhaust all
+	// the filehandles or network connections.
+
+	th := make(chan bool, workers)
+
+	for i := 0; i < workers; i++ {
+		th <- true
+	}
+
+	wg := new(sync.WaitGroup)
+
+	p.logger.Debug("refill poll w/ %d integers and %d workers", todo, workers)
+
+	success := 0
+	failed := 0
 
 	for j := 0; int64(j) < todo; j++ {
 
+		// Wait for the throttle to open a slot. Also record whether
+		// the operation was successful.
+
+		rsp := <-th
+
+		if rsp == true {
+			success += 1
+		} else {
+			failed += 1
+		}
+
+		// First check that we still actually need to keep fetching integers
+
+		if p.pool.Length() >= p.minpool {
+			p.logger.Debug("pool is full (%d) stopping after %d iterations", p.pool.Length(), j)
+			break
+		}
+
+		// Standard work group stuff
+
 		wg.Add(1)
 
-		go func(pr *Proxy) {
+		// Sudo make me a sandwitch. Note the part where we ping the throttle with
+		// the return value at the end both to signal an available slot and to record
+		// whether the integer harvesting was successful.
 
+		go func(pr *Proxy) {
 			defer wg.Done()
-			pr.AddToPool()
+			th <- pr.AddToPool()
 		}(p)
 	}
 
+	// More standard work group stuff
+
 	wg.Wait()
+
+	// Again note the way we are freeing a spot in the refill queue
+
+	p.refill <- true
+
+	t2 := time.Since(t1)
+	p.logger.Info("time to refill the pool with %d integers (success: %d failed: %d): %v (pool length is now %d)", todo, success, failed, t2, p.pool.Length())
+
 }
 
 func (p *Proxy) AddToPool() bool {
@@ -99,6 +189,7 @@ func (p *Proxy) GetInteger() (int64, error) {
 	i, err := p.client.CreateInteger()
 
 	if err != nil {
+		p.logger.Error("failed to create new integer, because %v", err)
 		return 0, err
 	}
 
@@ -109,6 +200,10 @@ func (p *Proxy) GetInteger() (int64, error) {
 func (p *Proxy) Integer() (int64, error) {
 
 	if p.pool.Length() == 0 {
+
+		go p.RefillPool()
+
+		p.logger.Warning("pool length is 0 so fetching integer from source")
 		return p.GetInteger()
 	}
 
@@ -129,7 +224,7 @@ func (p *Proxy) Integer() (int64, error) {
 func main() {
 
 	var port = flag.Int("port", 8080, "Port to listen")
-	var min = flag.Int("min", 1, "The minimum number of Brooklyn Integers to keep on hand at all times")
+	var min = flag.Int("min", 5, "The minimum number of Brooklyn Integers to keep on hand at all times")
 	var loglevel = flag.String("loglevel", "info", "Log level")
 	var cors = flag.Bool("cors", false, "Enable CORS headers")
 
@@ -148,7 +243,7 @@ func main() {
 		i, err := proxy.Integer()
 
 		if err != nil {
-		   	msg := fmt.Sprintf("Failed to retrieve integer because %v", err)
+			msg := fmt.Sprintf("Failed to retrieve integer because %v", err)
 			http.Error(rsp, msg, http.StatusBadRequest)
 		}
 
