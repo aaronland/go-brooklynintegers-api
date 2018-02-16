90a6a0fd3df6f5aa002b17a7d4fb302034ea3068
straup <straup@gmail.com>
2018-02-16 08:49:42 -0800

    add timings

--- cmd/int.go
+++ cmd/int.go
@@ -3,17 +3,22 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"github.com/aaronland/go-brooklynintegers-api"
 	"io"
 	"log"
 	"os"
 	"strconv"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 func main() {
 
 	var count = flag.Int("count", 1, "The number of Brooklyn Integers to mint.")
+	var clients = flag.Int("clients", 10, "...")
+	var timings = flag.Bool("timings", false, "...")
 
 	flag.Parse()
 
@@ -29,45 +34,63 @@ func main() {
 	wg := new(sync.WaitGroup)
 	mu := new(sync.Mutex)
 
-	t := 10
+	t := *clients
 	throttle := make(chan bool, t)
 
 	for i := 0; i < t; i++ {
-
 		throttle <- true
 	}
 
-	for i := 0; i < *count; i++ {
+	t1 := time.Now()
 
+	i := int32(0)
+
+	for atomic.LoadInt32(&i) < int32(*count) {
+
+		<-throttle
 		wg.Add(1)
 
 		go func(throttle chan bool) {
-
-			<-throttle
 
 			defer func() {
 				throttle <- true
 				wg.Done()
 			}()
 
-			i, err := client.CreateInteger()
+			if atomic.LoadInt32(&i) >= int32(*count) {
+				return
+			}
+
+			bi, err := client.CreateInteger()
 
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
 
 			mu.Lock()
 
-			str_i := strconv.Itoa(int(i))
+			str_i := strconv.Itoa(int(bi))
+
 			writer.WriteString(str_i + "\n")
 			writer.Flush()
 
 			mu.Unlock()
+
+			atomic.AddInt32(&i, 1)
 
 		}(throttle)
 
 	}
 
 	wg.Wait()
+
+	t2 := time.Since(t1)
+
+	if *timings {
+		t := fmt.Sprintf("time to mint %d integers: %v\n", *count, t2)
+		os.Stdout.Write([]byte(t))
+	}
+
 	os.Exit(0)
 }
